d3b302347d75c3136ccb0039ec0eaac48f837351
bigreni <bigreni@gmail.com>
2015-02-25 23:18:36 -0800

    Revert "One more test"
    This reverts commit 7940f46d06e5e56b76e8d5c28cdcea7791538cab.

--- Proverbs/www/js/Admob.js
+++ Proverbs/www/js/Admob.js
@@ -1,57 +1,34 @@
-var admobid = {};
-    if( /(android)/i.test(navigator.userAgent) ) { // for android
-        admobid = {
-            banner: 'ca-app-pub-9249695405712287/3154959152', // or DFP format "/6253334/dfp_example_ad"
-            interstitial: 'ca-app-pub-9249695405712287/3301233156'
-        };
-    } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
-        admobid = {
-            banner: 'ca-app-pub-xxx/zzz', // or DFP format "/6253334/dfp_example_ad"
-            interstitial: 'ca-app-pub-xxx/kkk'
-        };
-    } else { // for windows phone
-        admobid = {
-            banner: 'ca-app-pub-xxx/zzz', // or DFP format "/6253334/dfp_example_ad"
-            interstitial: 'ca-app-pub-xxx/kkk'
-        };
-    }
-
 function initAds() {
-//      if (admob) {
-//        var adPublisherIds = {
-//          ios : {
-//            banner : "ca-app-pub-8440343014846849/3119840614",
-//            interstitial : "ca-app-pub-8440343014846849/4596573817"
-//          },
-//          android : {
-//			banner: 'ca-app-pub-9249695405712287/3154959152',
-//			interstitial: 'ca-app-pub-9249695405712287/3301233156'
-//          }
-//        };
+      if (admob) {
+        var adPublisherIds = {
+          ios : {
+            banner : "ca-app-pub-8440343014846849/3119840614",
+            interstitial : "ca-app-pub-8440343014846849/4596573817"
+          },
+          android : {
+			banner: 'ca-app-pub-9249695405712287/3154959152',
+			interstitial: 'ca-app-pub-9249695405712287/3301233156'
+          }
+        };
 
-//        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
 
-//        admob.setOptions({
-//          publisherId:      admobid.banner,
-//          interstitialAdId: admobid.interstitial,
-//          autoShowBanner: true
-//        });
+        admob.setOptions({
+          publisherId:      admobid.banner,
+          interstitialAdId: admobid.interstitial,
+          autoShowBanner: true
+        });
 
-//        alert("Creating Banner");
-//        admob.createBannerView();
+        alert("Creating Banner");
+        admob.createBannerView();
 
-//      } else {
-//        alert("AdMobAds plugin not ready");
-//      }
-    if(AdMob) AdMob.createBanner( {
-        adId: admobid.banner, 
-        position: AdMob.AD_POSITION.TOP_CENTER, 
-        autoShow: true } );
-   }
+      } else {
+        alert("AdMobAds plugin not ready");
+      }
+    }
 
     function onDeviceReady() {
       initAds();
-
     }
 
     function onLoad(){
