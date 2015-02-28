6d3638cf86b371856078728c126548b5964d97cc
bigreni <bigreni@gmail.com>
2015-02-27 17:46:32 -0800

    Final Commit

---
+++ www/js/Admob.js
@@ -0,0 +1,35 @@
+function initAds() {
+      if (admob) {
+        var adPublisherIds = {
+          ios : {
+            banner : "ca-app-pub-8440343014846849/3119840614",
+            interstitial : "ca-app-pub-8440343014846849/4596573817"
+          },
+          android : {
+			banner: 'ca-app-pub-9249695405712287/3154959152',
+			interstitial: 'ca-app-pub-9249695405712287/3301233156'
+          }
+        };
+
+        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+
+        admob.setOptions({
+          publisherId:      admobid.banner,
+          interstitialAdId: admobid.interstitial,
+          autoShowBanner: true
+        });
+
+      } else {
+        alert("AdMobAds plugin not ready");
+      }
+    }
+
+    function onDeviceReady() {
+      initAds();
+      alert("Creating Banner");
+      admob.createBannerView();
+    }
+
+    function onLoad(){
+    document.addEventListener("deviceready", onDeviceReady, false);        
+    }
